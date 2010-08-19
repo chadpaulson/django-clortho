f6e3c67e83c4c2aa5682888acdadc6d0b6c3fde3
Greg Taylor <gtaylor@duointeractive.com>
2010-08-19 00:21:48 -0400

    Tab removal, formatting, and adding a message at time of login.

--- clortho/urls.py
+++ clortho/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('clortho', 
-	url(r'^on-facebook-authentication/$', 'views.on_facebook_authentication', name='on_facebook_authentication'),
+	url(r'^on-facebook-authentication/$', 'views.on_facebook_authentication', 
+        name='on_facebook_authentication'),
 )
