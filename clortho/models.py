c180d633a50da5f2d86f483f0056b4260a07ec26
Greg Taylor <gtaylor@duointeractive.com>
2010-08-27 15:35:54 -0400

    Adding admin for the model

--- clortho/models.py
+++ clortho/models.py
@@ -2,10 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Facebook(models.Model):
-
-	user = models.ForeignKey(User, related_name='facebook')
-	uid = models.CharField(max_length=20, unique=True)
-	url = models.URLField(blank=True, null=True)
-	
-	def __unicode__(self):
-		return u'%s %s\'s Facebook Profile' % (self.user.first_name, self.user.last_name)
+    user = models.ForeignKey(User, related_name='facebook')
+    uid = models.CharField(max_length=20, unique=True)
+    url = models.URLField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "Facebook User"
+        verbose_name_plural = "Facebook Users"
+    
+    def __unicode__(self):
+        return u'%s %s\'s Facebook Profile' % (self.user.first_name, 
+                                               self.user.last_name)
