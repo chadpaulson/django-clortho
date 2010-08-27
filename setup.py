c426c718f5383babdfbe533f6637ea344d37c4ba
Greg Taylor <gtaylor@duointeractive.com>
2010-08-27 12:53:41 -0400

    Packaging stuff needed for pip deployments.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import athumb
+
+long_description = open('README.md').read()
+
+setup(
+    name='django-clortho',
+    version=athumb.VERSION,
+    packages=['clortho'],
+    description='A Django authentication backend for Facebook via Open Graph.',
+    author='Chad Paulson',
+    license='BSD License',
+    url='http://github.com/chadpaulson/django-clortho',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
