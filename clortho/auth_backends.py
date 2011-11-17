8dae6ce88b504fc94bf73a0552d056ab6a98fd69
Chad Paulson <chad.paulson@gmail.com>
2011-11-17 09:49:15 -0600

    Fixing late night exception typos.

--- clortho/auth_backends.py
+++ clortho/auth_backends.py
@@ -14,12 +14,12 @@ class ClorthoBackend(ModelBackend):
     def authenticate(self, service, key):
         try:
             return Keymaster.objects.get(service=service,key=key).user
-        except DoesNotExist:            
+        except Keymaster.DoesNotExist:            
             return None
     
     def get_user(self, user_id):
         try:
             return ClorthoUser.objects.get(pk=user_id)
-        except DoesNotExist:
+        except ClorthoUser.DoesNotExist:
             return None
 
