c2d17461533ccdb5eb29259eb7cd2b5f6164174f
Chad Paulson <chad.paulson@gmail.com>
2012-01-31 21:51:50 -0600

    Cleaned up Greg's initial facebook context processor contribution.
    Wrapped up Facebook implementation cleanup.  More to come after I rough in OpenId and OAuth support.

--- clortho/context_processors.py
+++ clortho/context_processors.py
@@ -3,8 +3,11 @@ from django.conf import settings
 
 def facebook(request):
     """
-    Returns common context stuff for use in HTML templates.
+    Adds Facebook-specific context variables to context.
     """
     return {
-        'FACEBOOK_APP_ID': settings.FACEBOOK_APP_ID,
+        'FACEBOOK_APP_ID': getattr(settings, 'FACEBOOK_APP_ID', None),
+        'FACEBOOK_SECRET_KEY': getattr(settings, 'FACEBOOK_SECRET_KEY', None),
+        'FACEBOOK_PERMS': getattr(settings, 'FACEBOOK_EXTENDED_PERMISSIONS',
+            None),
     }
